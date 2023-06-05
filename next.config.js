5b1dacb7b0a3dd118e6eb58f6697da7b96e5855b
hedevelope <batujano@gmail.com>
2023-06-05 16:48:39 +0300

    reupload

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["upcdn.io", "replicate.delivery", "lh3.googleusercontent.com"],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/github",
+        destination: "https://github.com/hedevelope/arcroom",
+        permanent: false,
+      },
+    ];
+  },
+};
